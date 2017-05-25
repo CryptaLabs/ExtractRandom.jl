748d6dc4adaf2212e31908c0976321d306be9f43
Mayeul d'Avezac <mayeul@cryptalabs.com>
2017-05-25 21:31:20 +0100

    First trial for two_universal function

--- src/ExtractRandom.jl
+++ src/ExtractRandom.jl
@@ -1,5 +1,136 @@
 module ExtractRandom
+using ArgCheck
+export two_universal, two_universal!, poisson, poisson_min_entropy, bit_representation,
+       integer_representation
 
-# package code goes here
+""" Poisson distribution """
+poisson(events::Number, average::Number) = e^-average * average^events / factorial(events)
+""" Min entropy for a poisson distribution with mean n """
+poisson_min_entropy(average::Number; basis::Real=e) = begin
+    -log(poisson(average, average)) / log(basis)
+end
+
+"""
+```Julia
+two_universal!(a::AbstractMatrix{Bool}, v::AbstractVector{Bool}, out::AbstractVector{Bool})
+```
+
+Applies the two universal hashing function (parameterized by `a`) to the vector `v`.
+"""
+two_universal!(a::AbstractMatrix{Bool}, v::AbstractVector{Bool},
+               out::AbstractVector{Bool}) = begin
+    @argcheck length(v) % size(a, 2) == 0
+    @argcheck length(out) % size(a, 1)== 0
+    @argcheck size(a, 1) * length(v) == size(a, 2) * length(out)
+    vⱼ = v[1]
+    n, m = size(a)
+    for i in 1:length(out)
+        out[i] = vⱼ & a[(i - 1) % n + 1, 1]
+    end
+    for j in 2:length(v)
+        vⱼ = v[j]
+        for i in 1:length(out)
+            out[i] $= vⱼ & a[(i - 1) % n + 1, (j - 1) % m + 1]
+        end
+    end
+    out
+end
+
+two_universal(a::AbstractMatrix{Bool}, v::AbstractVector{Bool}) = begin
+    @argcheck length(v) % size(a, 2) == 0
+    n = div(length(v), size(a, 2)) * size(a, 1)
+    two_universal!(a, v, similar(v, (n,)))
+end
+
+two_universal{T <: Integer}(a::AbstractMatrix{T}, v::AbstractVector{T}) = begin
+    @argcheck length(v) % size(a, 2) == 0
+    n = round(Int64, (div(length(v), size(a, 2)) * size(a, 1)) / 8sizeof(T), RoundUp)
+    two_universal!(a, v, similar(v, (n,)))
+end
+
+two_universal!{T <: Integer}(a::AbstractArray{T}, v::AbstractVector{T},
+                             out::AbstractVector{T}) = begin
+
+    Nbits = 8sizeof(eltype(out))
+    multiples = div(length(v), size(a, 2))
+    @argcheck length(a) ≠ 0
+    if length(v) == 0
+        fill!(out, zero(T))
+        return out
+    end
+    @argcheck length(v) % size(a, 2) == 0
+    @argcheck round(Int64, (multiples * size(a, 1)) / Nbits, RoundUp) == length(out)
+
+
+    vⱼ = v[1]
+    n, m = size(a)
+    nbitsy(i) = min(Nbits, size(a, 1) * multiples  - i * Nbits) - 1
+    for i in 0:(length(out) - 1)
+        current = zero(T)
+        for i′ in 0:nbitsy(i)
+            intermediate = vⱼ & a[(i * Nbits + i′) % n + 1, 1]
+            current |= convert(T, xorbits(intermediate)) << (Nbits - 1 - i′)
+        end
+        out[i + 1] = current
+    end
+
+    for j in 2:length(v)
+        vⱼ = v[j]
+        for i in 0:(length(out) - 1)
+            current = out[i + 1]
+            for i′ in 0:nbitsy(i)
+                intermediate = vⱼ & a[(i * Nbits + i′) % n + 1, (j - 1) % m + 1]
+                current $= convert(T, xorbits(intermediate)) << (Nbits - 1 - i′)
+            end
+            out[i + 1] = current
+        end
+    end
+    out
+end
+
+@inline bget(a::Integer, i::Integer) = Bool(a & (1 << i))
+
+xorbits(a::Integer) = begin
+    result::Bool = a & 1
+    for i in 1:(sizeof(a) * 8 - 1)
+        result $= (a & (1 << i)) ≠ 0
+    end
+    result::Bool
+end
+
+"""
+    integer_representation{T < Integer}(::Type{T}, bits::AbstractArray{Bool})
+
+Creates array with same bit representation as input bitstring
+"""
+integer_representation{T <: Integer}(::Type{T}, bits::AbstractArray{Bool}) = begin
+    nbits = 8sizeof(T)
+    N = round(Int64, length(bits) / nbits, RoundUp)
+    result = zeros(T, N)
+    for i in eachindex(bits)
+        j = div(i - 1, nbits) + 1
+        j′ = nbits - 1 - (i - 1) % nbits
+        result[j] |= convert(T, bits[i]) << j′
+    end
+    result
+end
+
+"""
+    bit_representation(input::AbstractArray)
+
+Creates bit string with same bit representation as input array. The first dimension of the
+result is expanded by `8sizeof(eltype(input))`.
+"""
+bit_representation(input::AbstractArray) = begin
+    const T = eltype(input)
+    const nbits = 8sizeof(T)
+    result = BitArray(size(input, 1) * nbits, size(input)[2:end]...)
+    for i in eachindex(input)
+        for i′ in 0:(nbits - 1)
+            result[(i - 1) * nbits + nbits - i′] = (input[i] & (1 << i′)) ≠ 0
+        end
+    end
+    result
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,87 @@
+module ExtractRandomTest
 using ExtractRandom
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "Poisson" begin
+    # Sanguetti, et al, PRX 4, 031056 (2014), just because
+    @test poisson_min_entropy(big(1.5e4), basis=2) ≈ 8.2620935198439106576388550
+    @test poisson_min_entropy(big(410), basis=2) ≈ 5.66578134513010429986151
+end
+
+@testset "2 universal hashing" begin
+    @testset "With bits" begin
+        a = convert(BitMatrix, [false true; true true; false false])
+        v = convert(BitVector, [false, true])
+        @test two_universal(a, v) == [true, true, false]
+
+        a = [true true true; true false true; false false true]
+        v = [false, true, true]
+        @test two_universal(a, v) == [false, true, true]
+    end
+
+    @testset "xor bits of an integer" begin
+        Nbits = sizeof(typeof(1)) - 1
+        @test ExtractRandom.xorbits(0) == false
+        for i in 0:Nbits
+            @test ExtractRandom.xorbits(1 << i) == true
+            for j in (i + 1):Nbits
+                @test ExtractRandom.xorbits((1 << j) | (1 << i)) == false
+                for k in (j + 1):Nbits
+                    @test ExtractRandom.xorbits((1 << j) | (1 << i) | (1 << k)) == true
+                end
+            end
+        end
+    end
+
+    @testset "bit and integer representation" begin
+
+        @test bit_representation([1]) == BitArray(vcat(zeros(Integer, 63), [1]))
+        @test bit_representation([3]) == BitArray(vcat(zeros(Int64, 62), [1, 1]))
+        @test bit_representation([6]) == BitArray(vcat(zeros(Int64, 61), [1, 1, 0]))
+        expected = BitArray(vcat(zeros(Int64, 61), [1, 1, 0], zeros(Integer, 63), [1]))
+        @test bit_representation([6, 1]) == expected
+
+        @test integer_representation(Int64, bit_representation([1])) == [1]
+        @test integer_representation(Int64, bit_representation([2])) == [2]
+        @test integer_representation(Int64, bit_representation([3])) == [3]
+        @test integer_representation(Int64, bit_representation([-3])) == [-3]
+
+        a = rand(Int64, (rand(1:100, )))
+        @test integer_representation(Int64, bit_representation(a)) == a
+    end
+
+    @testset "With bytes" begin
+        @testset "Just one block (no modulo)" begin
+            a = UInt8[1 2; 3 4]
+            v = UInt8[1, 1]
+            @test two_universal(a, v) == [(UInt8(1) << 7 | (UInt8(1) << 6))]
+
+            const xorbits = ExtractRandom.xorbits
+            a = UInt8[1 2; 3 4]
+            v = UInt8[1, 3]
+            expected = (xorbits((1 & 1) | (2 & 3)) << 7 | (xorbits((1 & 3) | (3 & 4)) << 6))
+            @test two_universal(a, v) == [expected]
+        end
+
+        @testset "vs bit arrays" begin
+            a = rand(Int8, (rand(1:100, 2)...))
+            v = rand(Int8, 3size(a, 2))
+            abool = bit_representation(a)
+            abool = reshape(abool, (8sizeof(eltype(a)), size(a)...))
+            abool = permutedims(abool, [2, 1, 3:ndims(abool)...])
+            abool = reshape(abool, (size(a, 1), 8sizeof(eltype(a)) * size(a, 2)))
+
+            # makes sure the matrix is represented correctly
+            for i in 1:size(a, 1)
+                @test integer_representation(Int8, abool[i, :]) == a[i, :]
+            end
+
+            vbool = bit_representation(v)
+
+            x = two_universal(a, v)
+            xbool = two_universal(abool, vbool)
+            @test bit_representation(x)[1:length(xbool)] == xbool
+        end
+    end
+end
+end
