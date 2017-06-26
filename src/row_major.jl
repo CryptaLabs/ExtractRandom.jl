f3917fca2aee8dae4d007925fb749fef75cf089d
Mayeul d'Avezac <mayeul@cryptalabs.com>
2017-06-26 11:05:50 +0100

    :lipstick: one less local variable

--- src/row_major.jl
+++ src/row_major.jl
@@ -10,10 +10,11 @@ row_major_two_universal!{T <: Integer}(at::AbstractMatrix{T}, v::AbstractVector{
     @argcheck length(v) % size(at, 1) == 0
     @argcheck round(Int64, (multiples * size(at, 2)) / nbits, RoundUp) == length(out)
 
+    Nrange = 2:size(at, 1)
     for mult in 0:(multiples - 1), j in 1:size(at, 2)
         i′₀ = mult * size(at, 1) + 1
         x = at[1, j] & v[i′₀]
-        for i in 2:size(at, 1)
+        for i in Nrange
             x $= at[i, j] & v[i′₀ + i - 1]
         end
         ibit = mult * size(at, 2) + j
